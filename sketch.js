1115d5276ee67860b79187938d90b150fd34ac98
Saanvi-13 <79588400+Saanvi-13@users.noreply.github.com>
2021-07-17 15:18:28 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 var enemy1Img,enemy2Img,enemy3Img,shooterImg;
 var noonImg,nightImg,eveningImg,morningImg;
 var enemy,shooter;
+var bullet;
 var enemyGroup;
 var score = 0;
 
@@ -12,6 +13,7 @@ function preload(){
     shooterImg=loadImage("images/shooter.png");
     shooter2Img=loadImage("images/shooter2.png");
     shooter3Img=loadImage("images/shooter3.png");
+    bulletImg=loadImage("images/bullet.png");
 
    
     nightImg=loadImage("images/night.jpg");
@@ -22,7 +24,7 @@ function preload(){
 }
 
 function setup(){
-    canvas = createCanvas(1240,572);
+    canvas = createCanvas(1240,600);
    
     // enemy1 = createSprite(400,250,50,50);
     // enemy1.addImage(enemy1Img);
@@ -36,9 +38,15 @@ function setup(){
     // enemy3.addImage(enemy3Img);
     // enemy3.scale = 0.2
 
-    shooter = createSprite(600,420,50,50);
+    shooter = createSprite(600,500,50,50);
     shooter.addImage(shooterImg);
     shooter.scale = 1
+
+    bullet = createSprite(740,500);
+    bullet.addImage(bulletImg);
+    bullet.visible = false;
+
+
 
     enemyGroup = new Group();
 
@@ -57,15 +65,31 @@ function draw(){
 
 if(keyDown("LEFT_ARROW")){
 shooter.addImage(shooter2Img);
+bullet.visible = true;
+
+if (keyDown("UP_ARROW")){
+    bullet.velocityX=2;
+    enemyGroup.destroyEach();
+    
+}
 }
 
 if(keyDown("RIGHT_ARROW")){
     shooter.addImage(shooter3Img);
+    bullet.visible = true;
+    if (keyDown("UP_ARROW")){
+        bullet.velocityX=2;
+        
+        
+    }
     }
 
 if(keyDown("DOWN_ARROW")){
     shooter.addImage(shooterImg);
      }
+
+
+
 
 spawnEnemy();
 
@@ -74,13 +98,13 @@ drawSprites();
 
 function spawnEnemy(){
     //for(var i = 0;i<3;i++){
-       // w = random(400,800);
-        //h = random(250,310);
-        if(frameCount%60===0){
+        // w = random(400,800);
+        // h = random(250,310);
+        if(frameCount%200===0){
 
         
-        enemy = createSprite(800,250);
-        
+        enemy = createSprite(800,400);
+        enemy.velocityX=-3;
         var rand = Math.round(random(1,3));
         switch(rand){
             case 1: enemy.addImage(enemy1Img);
@@ -90,8 +114,11 @@ function spawnEnemy(){
             case 3: enemy.addImage(enemy3Img);
             break;
         }
-        enemy.scale = 0.2;
+        enemy.scale = 0.4;
         enemyGroup.add(enemy);
+
+        enemy.depth = shooter.depth;
+        shooter.depth = shooter.depth+1;
     }
 
     }
